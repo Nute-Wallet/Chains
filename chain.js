f90de2c32ea584aa3488e2198836335b7d9b5ab7
rajneeshnadcab <rajneesnadcab@gmail.com>
2023-09-28 10:52:48 +0530

    first commit

---
+++ chain.js
@@ -0,0 +1,60 @@
+[
+    {
+      "chainId": "0x38",
+      "balance": "0",
+      "tokens": [],
+      "nfts": [],
+      "nativeCurrency": {
+        "name": "Binance Coin",
+        "symbol": "BNB",
+        "decimals": 18,
+        "address": "0x0000000000000000000000000000000000000000",
+        "slug": "bsc"
+      },
+      "show": true
+    },
+    {
+      "chainId": "0x1",
+      "balance": "0",
+      "tokens": [],
+      "nfts": [],
+      "nativeCurrency": {
+        "name": "Ethereum",
+        "symbol": "ETH",
+        "decimals": 18,
+        "address": "0x0000000000000000000000000000000000000000",
+        "slug": "ethereum"
+      },
+      "show": true
+    },
+    {
+      "chainId": "0x89",
+      "balance": "0",
+      "tokens": [],
+      "nfts": [],
+      "nativeCurrency": {
+        "name": "Matic",
+        "symbol": "MATIC",
+        "decimals": 18,
+        "address": "0x0000000000000000000000000000000000000000",
+        "slug": "polygon"
+      },
+      "show": true
+    },
+    {
+      "chainId": "0x61",
+      "balance": "0",
+      "tokens": [],
+      "nfts": [],
+      "nativeCurrency": {
+        "name": "Binance Coin",
+        "symbol": "BNB",
+        "decimals": 18,
+        "balance": "0",
+        "address": "0x0000000000000000000000000000000000000000",
+        "slug": "bsc_testnet"
+      },
+      "show": true
+    }
+  ]
+  
